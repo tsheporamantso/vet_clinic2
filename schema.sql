6e415ba9314feecb4d6e8049043db8ae634c0784
tsheporamantso <tshepo.ramantso@outlook.com>
2023-10-23 15:14:57 +0200

    Correct typo

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Day 1 create animals table */
 
 CREATE TABLE animals (
-    id INT BIGSERIAL NOT NULL PRIMARY KEY,
+    id BIGSERIAL NOT NULL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     date_of_birth DATE NOT NULL,
     escape_attempts INTEGER DEFAULT 0,
