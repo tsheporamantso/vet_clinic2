7eee5665ed99fdc3d3f149240fca1b36780dfbcf
tsheporamantso <tshepo.ramantso@outlook.com>
2023-10-24 14:42:59 +0200

    Add aggregate queries in conjunction with group by

--- queries.sql
+++ queries.sql
@@ -35,7 +35,7 @@ FROM animals
 WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
 /**
-* ! Day 2 Query and Update animals table
+* ! Day 2 SQL Transctions, to safely delete and update records.
 */
 
 BEGIN;
@@ -75,5 +75,27 @@ UPDATE animals SET weight_kg = -1 * weight_kg WHERE weight_kg < 0;
 COMMIT;
 SELECT * FROM animals;
 
+/**
+* ! Day 2 Aggregates in conjunction with Group By
+*/
 
+SELECT COUNT(*) FROM animals;
 
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT neutered, SUM(escape_attempts) AS total_escape_attempts
+FROM animals
+GROUP BY neutered
+ORDER BY total_escape_attempts DESC
+LIMIT 1;
+
+SELECT species, MIN(weight_kg), MAX(weight_kg)
+FROM animals
+GROUP BY species;
+
+SELECT species, AVG(escape_attempts) AS avg_escape_attempts
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
