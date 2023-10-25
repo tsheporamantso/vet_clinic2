faf26c6bd89d43a786d3aa2ddd1398c6b7f5f17c
tsheporamantso <tshepo.ramantso@outlook.com>
2023-10-25 12:48:41 +0200

    Update animals table and set species id from species table

--- data.sql
+++ data.sql
@@ -38,3 +38,12 @@ INSERT INTO species(name)
 VALUES
 ('Pokemon'),
 ('Digimon');
+
+UPDATE animals
+SET species_id = (CASE
+WHEN name LIKE '%mon' THEN (
+    SELECT id FROM species WHERE name = 'Digimon'
+) ELSE (
+    SELECT id FROM species WHERE name = 'Pokemon'
+)
+END);
