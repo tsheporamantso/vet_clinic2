10240b711659689d288f77739aa23b324e4d6db9
tsheporamantso <tshepo.ramantso@outlook.com>
2023-10-25 12:28:03 +0200

    Add owners and species tables and also altered animals table

--- schema.sql
+++ schema.sql
@@ -15,3 +15,25 @@ CREATE TABLE animals (
 
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(50);
+
+/**
+* ! Day 3 Query multiple tables
+*/
+
+CREATE TABLE owners(
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    full_name VARCHAR(255),
+    age INTEGER
+);
+
+CREATE TABLE species(
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD COLUMN species_id INTEGER REFERENCES species(id),
+ADD COLUMN owners_id INTEGER REFERENCES owners(id);
